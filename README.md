cc025bdab2adb2f922a61e24d78e64aee779e6e0
nielsmadan <nielsmadan@gmail.com>
2012-12-19 01:59:19 +0100

    update readme, implement function to run line of python into message.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 mercury
 =======
+
+This vim plugin provides functions to run some piece of code from a source (current line, selection, buffer) to some
+destination (message window, selection, register, new split window) in a variety of languages.
--- plugin/mercury.py
+++ plugin/mercury.py
@@ -1,3 +1,6 @@
+import re
+import subprocess
+
 import vim
 import venom
 
@@ -6,10 +9,26 @@ def run_python_file():
     vim.command("w !python<CR>")
 
 
-def append_hello_world():
-    print "APPENDING HELLO WORLD"
-    # vim.current.buffer.append("Hello World")
+def get_start_white_space(line):
+    pass
+    # return re.match(
 
 
-venom.nnoremap("<leader><f7>", append_hello_world)
-print "MERCURY HAS RUN"
+def remove_minimal_indent(lines):
+    pass
+    # minimal_indent = min([
+
+
+def run_python_line_to_message():
+    line = venom.get_current_line(read_only=True)
+    if not line.startswith("print"):
+        line = "print " + line
+
+    print subprocess.check_output(['python', '-c', line])
+
+# def append_hello_world():
+#     print "APPENDING HELLO WORLD"
+#     vim.current.buffer.append("Hello World")
+
+venom.nnoremap("<leader>r", run_python_line_to_message)
+print "MERCURY LOADED"
