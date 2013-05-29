07754ad257e572b8114b59eb1b05a0848ac06270
nielsmadan <nielsmadan@gmail.com>
2013-05-29 22:54:57 +0200

    exe: document and fix automatic print insertion for ruby (#1, #2).

--- plugin/mercury/ruby.py
+++ plugin/mercury/ruby.py
@@ -2,7 +2,8 @@ import subprocess
 
 
 def execute(expr, buff):
-    if not expr.startswith("print"):
+    if "\n" not in expr and not expr.startswith("print") and not expr.startswith("puts"):
         expr = "print " + expr
-
-    return subprocess.check_output(['ruby', '-e', expr])
+        return subprocess.check_output(['ruby', '-e', expr]) + "\n"
+    else:
+        return subprocess.check_output(['ruby', '-e', expr])
--- test/test_ruby.py
+++ test/test_ruby.py
@@ -6,11 +6,21 @@ import mercury
 
 class TestRuby(unittest.TestCase):
     def test_execute_simple(self):
-        result = mercury.ruby.execute("puts 'this'", "")
+        result = mercury.ruby.execute("puts 'Hello, World'", "")
 
-        eq_(result, "this\n")
+        eq_(result, "Hello, World\n")
+
+    def test_execute_assignment(self):
+        result = mercury.ruby.execute("x = 5", "")
+
+        eq_(result, "5\n")
+
+    def test_execute_list(self):
+        result = mercury.ruby.execute("[1, 2, 3]", "")
+
+        eq_(result, "[1, 2, 3]\n")
 
     def test_execute_simple_multiple(self):
-        result = mercury.ruby.execute("puts 'this'\nputs 'that'", "")
+        result = mercury.ruby.execute("puts 'Hello, World'\nputs 'Goodbye, World'", "")
 
-        eq_(result, "this\nthat\n")
+        eq_(result, "Hello, World\nGoodbye, World\n")
